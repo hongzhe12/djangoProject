9b2df3fb0935a6f6f96f8c9b044fd260fc819138
hongzhe12 <hongzhe2022@163.com>
2026-02-20 08:06:50 +0000

    feat(djangoProject): 添加mptt和mdeditor应用并更新CSRF信任源配置
    - 在INSTALLED_APPS中添加了mptt和mdeditor依赖包
    - 更新CSRF_TRUSTED_ORIGINS配置，将虚拟机IP地址更新为https协议
    - 为CSRF配置项添加注释说明

--- djangoProject/local_settings.py
+++ djangoProject/local_settings.py
@@ -1,5 +1,6 @@
-
 INSTALLED_APPS = [
+    "mptt",
+    "mdeditor",
     "django_mailbox",
     "article.apps.ArticleConfig",
     "users.apps.UsersConfig",
@@ -8,4 +9,7 @@ INSTALLED_APPS = [
 DEBUG = False  # 切换为生产环境
 
 # ==================== 跨域和 CSRF 配置 ====================
-CSRF_TRUSTED_ORIGINS = ["http://192.168.204.128:80", "https://192.168.204.128"]
+CSRF_TRUSTED_ORIGINS = [
+    # 虚拟机IP
+    "https://192.168.159.128",
+]
